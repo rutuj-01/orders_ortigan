52ebf3dae4a9f8ba38d2d61b60d453dfa1acf7cb
Rutuj Bhakre <rbhakre99@gmail.com>
2020-10-20 10:14:11 +0530

    login page

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LOGIN</title>
+</head>
+<body>
+	<form action="sql_login.php" method="post">
+		<label>Email</label>
+		<br>
+		<input type="email" name="email">
+		<br>
+		<label>Password</label>
+		<br>
+		<input type="Password" name="password">
+		<br>
+		<input type="submit" name="submit" value="submit">
+	</form>
+
+
+</body>
+</html>
